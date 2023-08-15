6078b426b5d4514fad3dbf26c5257300261708ae
Constantin Sabin <sabinconstantin60@yahoo.com>
2023-08-15 17:37:26 +0300

    added streamfield

--- generic/models.py
+++ generic/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 class GenericPage(Page):
     banner_title = models.CharField(max_length=100, default="Welcome to my generic page")
@@ -25,11 +28,22 @@ class GenericPage(Page):
         related_name="+",
     )
     
+    body = StreamField([
+    # ('name', blocks.SomethjngBlock()),)   
+    ('heading', blocks.CharBlock(template="heading_block.html")),
+    ('image', ImageChooserBlock()),
+    ('paragraph', blocks.RichTextBlock()),
+    ], 
+    null=True,
+    use_json_field=True,
+    )
+    
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
         FieldPanel("introduction"),
         FieldPanel("banner_image"),
         FieldPanel("author"),
+        FieldPanel("body"),
     ]
 
     
@@ -48,12 +62,15 @@ class Author(models.Model):
         related_name="+",
     )
     
+    
+    
     panels = [
         FieldPanel("name"),
         FieldPanel("title"),
         FieldPanel("company_name"),
         FieldPanel("company_url"),
         FieldPanel("image"),
+        
     ]
     
     def __str__(self):
